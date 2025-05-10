29f76074a5808b96adf88ffee0406aa9ea7afa2a
Milan Canty <milancanty@gmail.com>
2025-05-10 17:05:29 -0400

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Milan Canty on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
